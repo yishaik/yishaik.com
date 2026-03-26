19e69e12b916483a7b656334c93038bc513c732c
yishaik <47695058+yishaik@users.noreply.github.com>
2026-03-26 23:41:41 +0200

    fix: improve contact flow and add crawl basics

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,23 @@
+import type { APIRoute } from 'astro';
+
+const urls = [
+  'https://yishaik.com/',
+  'https://yishaik.com/he/',
+];
+
+export const GET: APIRoute = () => {
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map(
+    (url) => `  <url><loc>${url}</loc></url>`
+  )
+  .join('\n')}
+</urlset>`;
+
+  return new Response(body, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+    },
+  });
+};
